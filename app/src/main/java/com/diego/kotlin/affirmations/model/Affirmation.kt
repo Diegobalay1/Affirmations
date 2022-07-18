1d716196a0acbc96fb65cd4f26ac84080ea188e7
balay <dlolhd@gmail.com>
2022-07-18 08:32:45 +0200

    Creación de la data class Affirmation.kt en el paquete model

---
+++ app/src/main/java/com/diego/kotlin/affirmations/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.diego.kotlin.affirmations.model
+
+/**
+ * En el paquete model:
+ * Los desarrolladores a menudo usan model como el nombre del paquete
+ * para las clases que modelan (o representan) los datos.
+ */
+data class Affirmation(val stringResourceId: Int) {
+}
