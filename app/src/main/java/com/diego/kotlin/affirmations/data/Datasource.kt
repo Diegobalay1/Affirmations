517320d263df23d5c6e6ddd15075f7aa7c08fa2d
balay <dlolhd@gmail.com>
2022-07-18 08:41:19 +0200

    Creación de la clase DataSource.kt que prepara la fuente de datos.

--- app/src/main/java/com/diego/kotlin/affirmations/data/Datasource.kt
+++ app/src/main/java/com/diego/kotlin/affirmations/data/Datasource.kt
@@ -1,4 +1,28 @@
 package com.diego.kotlin.affirmations.data
 
+import com.diego.kotlin.affirmations.R
+import com.diego.kotlin.affirmations.model.Affirmation
+
+/**
+ * En paquete data:
+ * Puede y debe ocultar esta preparación de datos en una clase de fuente de datos
+ * separada que prepara los datos para la aplicación.
+ */
 class Datasource {
+
+    fun loadAffirmations() : List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+
 }
