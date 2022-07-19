035b7249fde32c9c3743b0844c2c7fafded7dda8
balay <dlolhd@gmail.com>
2022-07-19 16:22:19 +0200

    Creación del adaptador para el recyclerview 'ItemAdapter'.

--- app/src/main/java/com/diego/kotlin/affirmations/MainActivity.kt
+++ app/src/main/java/com/diego/kotlin/affirmations/MainActivity.kt
@@ -2,10 +2,15 @@ package com.diego.kotlin.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.diego.kotlin.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
