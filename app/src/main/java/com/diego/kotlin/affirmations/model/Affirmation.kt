5acb91a019f98396be072ea66e5ac89973b3db9b
balay <dlolhd@gmail.com>
2022-07-21 09:58:48 +0200

    Añadimos parametro Int imageResourceId a clase Affirmation y usamos anotaciones de recursos.

--- app/src/main/java/com/diego/kotlin/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/diego/kotlin/affirmations/model/Affirmation.kt
@@ -1,9 +1,13 @@
 package com.diego.kotlin.affirmations.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 /**
  * En el paquete model:
  * Los desarrolladores a menudo usan model como el nombre del paquete
  * para las clases que modelan (o representan) los datos.
  */
-data class Affirmation(val stringResourceId: Int) {
-}
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int)
